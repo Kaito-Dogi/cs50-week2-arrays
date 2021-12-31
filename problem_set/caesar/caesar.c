f2d86935be9a15d1db34f80709d5a8f65c3613a5
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-31 15:03:21 +0000

    add: finish problem_set/caesar

--- problem_set/caesar/caesar.c
+++ problem_set/caesar/caesar.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
@@ -9,7 +10,7 @@ int main(int argc, string argv[])
     if (argc == 2)
     {
         // check if each char is digit
-        for (int i = 0, max = strlen(argv[1]); i < max; i++)
+        for (int i = 0, i_max = strlen(argv[1]); i < i_max; i++)
         {
             if (!isdigit(argv[1][i]))
             {
@@ -23,4 +24,25 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
+
+    // get key
+    int key = atoi(argv[1]);
+
+    // get plaintext
+    string plaintext = get_string("plaintext: ");
+
+    // encrypt plaintext
+    for (int i = 0, i_max = strlen(plaintext); i < i_max; i++)
+    {
+        if (plaintext[i] >= 'a' && plaintext[i] <= 'z')
+        {
+            plaintext[i] = (plaintext[i] - 'a' + key) % 26 + 'a';
+        }
+        else if (plaintext[i] >= 'A' && plaintext[i] <= 'Z')
+        {
+            plaintext[i] = (plaintext[i] - 'A' + key) % 26 + 'A';
+        }
+    }
+
+    printf("ciphertext: %s\n", plaintext);
 }
