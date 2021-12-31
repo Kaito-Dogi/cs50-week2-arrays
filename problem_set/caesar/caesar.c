f65abab560ff41b068e3dcdab0f0abdf2e0eecf4
Kaito-Dogi <kendobu0405@gmail.com>
2021-12-31 13:49:01 +0000

    add: check command line arguments

---
+++ problem_set/caesar/caesar.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    // check the number of command line arguments
+    if (argc == 2)
+    {
+        // check if each char is digit
+        for (int i = 0, max = strlen(argv[1]); i < max; i++)
+        {
+            if (!isdigit(argv[1][i]))
+            {
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+}
